ee6a539fe1e317a1212e8b3ee60c59ce6d18f7e5
izabayo7 <cedricizabayo7@gmail.com>
2021-03-07 20:03:12 +0200

    now controlling the duration

--- Main.py
+++ Main.py
@@ -15,16 +15,16 @@ class Main:
             now = time.localtime()
             if(now.tm_hour == int(self.time[0]) and now.tm_min == int(self.time[1])):
                 print("Harahiye kbx!!!")
-                test_sound = Playsound("./audio/sample.mp3")
+                test_sound = Playsound("./audio/sample.mp3",5)
                 test_sound.playSound()
-                time.sleep(60)
+                # time.sleep(60)
             else:
                 print(now.tm_hour,":",now.tm_min,":",now.tm_sec,"\n")
                 time.sleep(1)
 
 #Main execution 
 def main():
-    test = Main("19:48")
+    test = Main("19:58")
 
     test.run()
     
--- Playsound.py
+++ Playsound.py
@@ -3,19 +3,26 @@
 # @author Cedric & Souvede
 #
 
-from playsound import playsound
+from pygame import mixer
+import time
+
+mixer.init()
 
 class Playsound:
-    def __init__(self,path):
+    def __init__(self,path,duration):
         self.path = path
+        self.duration = duration
     
     def playSound(self):
-       sound_player = playsound(self.path)
+       print(self.path)
+       sound_player = mixer.Sound(self.path)
+       sound_player.play()
+       time.sleep(self.duration)
+       sound_player.stop()
         
-
-# #Main execution 
+#Main execution 
 # def main():
-#     test_sound = Playsound("./audio/sample.mp3")
+#     test_sound = Playsound("./audio/sample.mp3", 5)
 
 #     test_sound.playSound()
     
