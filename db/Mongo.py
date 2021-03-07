873de35c68a09c095118fb63433823a6b1199202
izabayo7 <cedricizabayo7@gmail.com>
2021-03-07 09:13:07 +0200

    modified main

--- db/Mongo.py
+++ db/Mongo.py
@@ -6,17 +6,22 @@
 from pymongo import MongoClient
 
 class Mongo:
-    def __init__(self,connection_string):
+    def __init__(self,connection_string,db_name):
         self.connection = MongoClient(connection_string)
+        self.db = self.connection[db_name]
     
     def getConnection(self):
         return self.connection
+    def getDbObject(self):
+        return self.db
 
-#Main execution
+#Main execution 
 def main():
-    test_conn = Mongo("mongodb://localhost/Kurious")
-    client = test_conn.getConnection()
-    # post = client.db.users.find()
-    print(client.list_database_names())
+    test_conn = Mongo("mongodb://localhost", 'Default')
+    # client = test_conn.getConnection()
+    mydb = test_conn.getDbObject()
+    mycol = mydb['users']
+    for x in mycol.find():
+        print(x)
     
 main()
