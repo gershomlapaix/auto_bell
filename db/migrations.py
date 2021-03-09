3608827390fcb9e26e779480422cf7137cab2372
Divin Irakiza <irakizadivin@gmail.com>
2021-03-09 13:18:15 +0200

    Db Restructure run migrations

--- db/migrations.py
+++ db/migrations.py
@@ -15,12 +15,44 @@ class Migration:
         self.cursor = self.connection.cursor()
 
     def run_migrations(self):
-        print('Running migrations ........')
+        print('\nRunning migrations ........\n')
         self.create_users_table()
+        self.create_bells_table()
+        self.create_timetables_table()
+        self.create_playsounds_table()
+
+    def create_timetables_table(self):
+        query = "CREATE TABLE IF NOT EXISTS timetables ( id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, " \
+                " day TEXT CHECK (day IN " \
+                "('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY', 'ALL_DAYS', " \
+                "'WEEK_DAYS', 'WEEKEND' )) NOT NULL, time TEXT NOT NULL, duration INTEGER DEFAULT 10, status  CHECK (" \
+                "status IN  ('ACTIVE', 'INACTIVE' )) DEFAULT 'ACTIVE', timestamps TIMESTAMP DEFAULT " \
+                "CURRENT_TIMESTAMP); "
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def create_playsounds_table(self):
+        query = "CREATE TABLE IF NOT EXISTS playsounds ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, " \
+                "path TEXT NOT NULL, status  CHECK (" \
+                "status IN  ('ACTIVE', 'INACTIVE' )) DEFAULT 'ACTIVE', timestamps TIMESTAMP DEFAULT " \
+                "CURRENT_TIMESTAMP); "
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def create_bells_table(self):
+        query = "CREATE TABLE IF NOT EXISTS bells ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, " \
+                "location TEXT NOT NULL, serial TEXT NOT NULL UNIQUE , status  CHECK (" \
+                "status IN  ('ACTIVE', 'INACTIVE' )) DEFAULT 'ACTIVE', timestamps TIMESTAMP DEFAULT " \
+                "CURRENT_TIMESTAMP); "
+        self.cursor.execute(query)
+        self.connection.commit()
 
     def create_users_table(self):
-        query = "CREATE TABLE IF NOT EXISTS users ( contact_id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, " \
-                " last_name TEXT NOT NULL, email TEXT NOT NULL UNIQUE,phone TEXT NOT NULL UNIQUE); "
+        query = "CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL " \
+                "UNIQUE, " \
+                "password TEXT NOT NULL, email TEXT NOT NULL UNIQUE , full_names TEXT NOT NULL, status CHECK (" \
+                "status IN  ('ACTIVE', 'INACTIVE' )) DEFAULT 'ACTIVE', timestamps TIMESTAMP DEFAULT " \
+                "CURRENT_TIMESTAMP); "
         self.cursor.execute(query)
         self.connection.commit()
 
